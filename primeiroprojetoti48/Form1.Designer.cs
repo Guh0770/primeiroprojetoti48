9eef08ce7828e9da851459ba634db0553db0a9e5
Gustavo Oliveira <gustavooliveiraxd430@gmail.com>
2025-11-11 17:07:31 -0300

    Calculadora atualizada

--- primeiroprojetoti48/Form1.Designer.cs
+++ primeiroprojetoti48/Form1.Designer.cs
@@ -28,99 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.Calculado = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.AD = new System.Windows.Forms.Button();
+            this.SUB = new System.Windows.Forms.Button();
+            this.MUL = new System.Windows.Forms.Button();
+            this.DIV = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(78, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 40);
+            this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(177, 230);
+            this.textBox2.Location = new System.Drawing.Point(479, 230);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(196, 40);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(498, 230);
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(103, 358);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(196, 40);
+            this.textBox3.Size = new System.Drawing.Size(562, 47);
             this.textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(151, 356);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(562, 47);
-            this.textBox4.TabIndex = 3;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(225, 465);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(55, 49);
-            this.textBox5.TabIndex = 3;
-            this.textBox5.Text = "  +";
             // 
             // Calculado
             // 
             this.Calculado.AutoSize = true;
             this.Calculado.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Calculado.Location = new System.Drawing.Point(246, 73);
+            this.Calculado.Location = new System.Drawing.Point(205, 78);
             this.Calculado.Name = "Calculado";
             this.Calculado.Size = new System.Drawing.Size(375, 73);
             this.Calculado.TabIndex = 4;
             this.Calculado.Text = "Calculadora";
             // 
-            // textBox1
+            // AD
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(304, 465);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(54, 49);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "  -";
+            this.AD.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AD.Location = new System.Drawing.Point(218, 449);
+            this.AD.Name = "AD";
+            this.AD.Size = new System.Drawing.Size(56, 52);
+            this.AD.TabIndex = 5;
+            this.AD.Text = "+";
+            this.AD.UseVisualStyleBackColor = true;
+            this.AD.Click += new System.EventHandler(this.AD_Click);
             // 
-            // textBox6
+            // SUB
             // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(381, 465);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(50, 49);
-            this.textBox6.TabIndex = 3;
-            this.textBox6.Text = " x";
+            this.SUB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SUB.Location = new System.Drawing.Point(297, 449);
+            this.SUB.Name = "SUB";
+            this.SUB.Size = new System.Drawing.Size(56, 52);
+            this.SUB.TabIndex = 5;
+            this.SUB.Text = "-";
+            this.SUB.UseVisualStyleBackColor = true;
+            this.SUB.Click += new System.EventHandler(this.SUB_Click);
             // 
-            // textBox7
+            // MUL
             // 
-            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(455, 465);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(44, 49);
-            this.textBox7.TabIndex = 3;
-            this.textBox7.Text = " /";
+            this.MUL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MUL.Location = new System.Drawing.Point(375, 449);
+            this.MUL.Name = "MUL";
+            this.MUL.Size = new System.Drawing.Size(56, 52);
+            this.MUL.TabIndex = 5;
+            this.MUL.Text = "X";
+            this.MUL.UseVisualStyleBackColor = true;
+            this.MUL.Click += new System.EventHandler(this.MUL_Click);
+            // 
+            // DIV
+            // 
+            this.DIV.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DIV.Location = new System.Drawing.Point(456, 449);
+            this.DIV.Name = "DIV";
+            this.DIV.Size = new System.Drawing.Size(56, 52);
+            this.DIV.TabIndex = 5;
+            this.DIV.Text = "/";
+            this.DIV.UseVisualStyleBackColor = true;
+            this.DIV.Click += new System.EventHandler(this.DIV_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1073, 606);
+            this.ClientSize = new System.Drawing.Size(789, 529);
+            this.Controls.Add(this.DIV);
+            this.Controls.Add(this.MUL);
+            this.Controls.Add(this.SUB);
+            this.Controls.Add(this.AD);
             this.Controls.Add(this.Calculado);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "f";
             this.ResumeLayout(false);
@@ -129,14 +137,14 @@
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label Calculado;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button AD;
+        private System.Windows.Forms.Button SUB;
+        private System.Windows.Forms.Button MUL;
+        private System.Windows.Forms.Button DIV;
     }
 }
 
--- primeiroprojetoti48/Form1.cs
+++ primeiroprojetoti48/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace primeiroprojetoti48
 {
@@ -17,14 +18,38 @@ namespace primeiroprojetoti48
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        private void AD_Click(object sender, EventArgs e)
         {
-            label1.Text = "Olá turma TI48";
+            double soma1 = double.Parse(textBox1.Text);
+            double soma2 = double.Parse(textBox2.Text);
+            double resultado = soma1 + soma2;
+            textBox3.Text = resultado.ToString();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void SUB_Click(object sender, EventArgs e)
         {
+            double subtracao1 = double.Parse(textBox1.Text);
+            double subtracao2 = double.Parse(textBox2.Text);
+            double resultado = subtracao1 - subtracao2;
+            textBox3.Text = resultado.ToString();
+        }
 
+        private void MUL_Click(object sender, EventArgs e)
+        {
+            double mult1 = double.Parse(textBox1.Text);
+            double mult2 = double.Parse(textBox2.Text);
+            double resultado = mult1 * mult2;
+            textBox3.Text = resultado.ToString();
+        }
+
+        private void DIV_Click(object sender, EventArgs e)
+        {
+            double div1 = double.Parse(textBox1.Text);
+            double div2 = double.Parse(textBox2.Text);
+            double resultado = div1 / div2;
+            textBox3.Text = resultado.ToString();
         }
     }
 }
